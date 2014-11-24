8837a06ca824b6725faa5c5b77b7c708c1a0737d
ChangHong_XXX <ChangHong_XXX@ChangHong>
2014-11-24 11:43:01 +0800

    修改图片地址

--- src/com/xqxy/lg/base/Url.java
+++ src/com/xqxy/lg/base/Url.java
@@ -14,5 +14,5 @@ public class Url {
 	public static final String URL_ORDER = "http://api2.xinlingmingdeng.com/order/";//订单
 	public static final String URL_MEMBER = "http://api2.xinlingmingdeng.com/member/";// 修改密码,找回密码
 	public static final String URL_DETAILS = "http://api2.xinlingmingdeng.com/details/";// 商品详情
-	public static final String URL_IMGPATH = "http://www.xinlingmingdeng.com";// 网络图片地址前缀
+	public static final String URL_IMGPATH = "http://legou.xinlingmingdeng.com";// 网络图片地址前缀
 }
